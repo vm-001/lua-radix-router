11f4f3038b1ea97fcfd75670735b8c2c7b67708c
Douglas-Lee <leeys.top@gmail.com>
2024-01-08 21:19:12 +0800

    perf: reduce memory usage

--- src/constants.lua
+++ src/constants.lua
@@ -9,6 +9,13 @@ return {
     variable = 2,
     catchall = 3,
   },
+  node_indexs = {
+    type = 1,
+    path = 2,
+    pathn = 3,
+    children = 4,
+    value = 5,
+  },
   -- The type of token
   token_types = {
     literal = 1,
